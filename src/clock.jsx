ae855455e923a47c0df531e6fc825db5c54f3ad9
Eddcakes <eddadamson@gmail.com>
2019-03-28 23:01:18 +0000

    inline styles -> scss added support for ie

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styles from './clock.module.scss'
 
 //move all clock set up functions into another file
 
@@ -19,13 +20,13 @@ function Clock(){
     };
   });
   return (
-    <div className="clock">
-        <div className="pulse" style={clockStyle}>
-          <span style={digitStyle}>
+    <div>
+        <div className={`${styles.clock} pulse`}>
+          <span className={styles.digits}>
             {`${clock.hours}:${clock.minutes}:${clock.seconds}`}
           </span>
-          <span style={tzStyle}>{clock.offset}</span>
-          <span style={ampmStyle}>{clock.ampm}</span>
+          <span className={styles.timezone}>{clock.offset}</span>
+          <span className={styles.ampm}>{clock.ampm}</span>
         </div>
     </div>
   );
@@ -36,35 +37,6 @@ export default Clock;
 /* https://github.com/facebook/react/issues/5783 -moz-fit-content
 ie doesnt like this implementation of grid. wewlad
 */
-const digitSize = '6rem'
-const clockDetailSize = '2rem'
-const clockStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)',
-  gridTemplateRows: '1fr 1fr',
-  gridTemplateAreas: "'main main main main' 'tz . . ampm'",
-  width: `calc(${digitSize} * 5)`,
-  margin: '0 auto',
-  fontFamily: '"Lucida Console", Monaco, monospace',
-}
-const digitStyle = {
-  gridArea: 'main',
-  fontSize: digitSize,
-}
-const ampmStyle ={
-  gridArea: 'ampm',
-  fontSize: clockDetailSize,
-  marginTop: '-1.2rem',
-  marginRight: '15%',
-  textAlign: 'right'
-}
-const tzStyle ={
-  gridArea: 'tz',
-  fontSize: clockDetailSize,
-  marginTop: '-1.2rem',
-  marginLeft: '15%',
-  textAlign: 'left'
-}
 const oneSecond = () => 1000
 const getCurrentTime = () => new Date()
 
